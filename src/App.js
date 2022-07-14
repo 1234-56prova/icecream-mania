ec93074d8c792ae1debb5de4b56d616abdd13260
Ilma <ilmaprova13@gmail.com>
2022-07-14 12:31:15 +0600

    daisy ui and initial code setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import About from './Pages/AboutUs/About';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home';
+import NotFound from './Pages/NotFound';
+import Footer from './Pages/Shared/Footer';
+import Header from './Pages/Shared/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/blogs' element={<Blogs />}></Route>
+        <Route path='/about-us' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
