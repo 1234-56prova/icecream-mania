a13ca44b797f0a0163d3148d2dbb17d2ae79f15b
Ilma <ilmaprova13@gmail.com>
2022-07-14 20:57:11 +0600

    class attribute changed to classname + footer

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div class="carousel w-full">
-            <div id="slide1" class="carousel-item relative w-full">
-                <img src="https://i.ibb.co/xXtsh4S/banner-1.png" class="w-full" />
-                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" class="btn btn-circle">❮</a>
-                    <a href="#slide2" class="btn btn-circle">❯</a>
+        <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/xXtsh4S/banner-1.png" className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide2" class="carousel-item relative w-full">
-                <img src="https://i.ibb.co/8PYY8JP/banner-2.png" class="w-full" />
-                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" class="btn btn-circle">❮</a>
-                    <a href="#slide3" class="btn btn-circle">❯</a>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/8PYY8JP/banner-2.png" className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide3" class="carousel-item relative w-full">
-                <img src="https://i.ibb.co/gMH4LFc/banner-3.png" class="w-full" />
-                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" class="btn btn-circle">❮</a>
-                    <a href="#slide4" class="btn btn-circle">❯</a>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/gMH4LFc/banner-3.png" className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Shop.js
+++ src/Pages/Home/Shop.js
@@ -11,7 +11,7 @@ const Shop = () => {
     }, [])
     return (
         <div >
-            <h1 className='text-center text-4xl text-lime-700 font-bold m-8'>Icecreams!</h1>
+            <h1 className='text-center text-4xl text-lime-700 font-bold m-12 font-serif'>Icecreams!</h1>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
                 {
                     icecreams.slice(0, 6).map(icecream => <IceCreams icecream={icecream}></IceCreams>)
