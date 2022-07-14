5c1baf47431b9fd03da080fec0a34043d8fea6e7
Ilma <ilmaprova13@gmail.com>
2022-07-14 20:14:53 +0600

    primary banner added

---
+++ src/Pages/Home/Discount.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Discount = () => {
+    return (
+        <div >
+            <h1 className='text-center text-4xl'>Discounts for the cutecreams!</h1> 
+            <h1>30% Discounts for these icecreams!</h1>
+             
+        </div>
+    );
+};
+
+export default Discount;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from './Banner';
+import Discount from './Discount';
 
 const Home = () => {
     return (
         <div>
-            
+            <Banner></Banner>
+            <Discount></Discount>
         </div>
     );
 };
