3f13a16f8a7cc84af43cd13e3e1490bbd4cae550
Ilma <ilmaprova13@gmail.com>
2022-07-15 11:42:40 +0600

    card updated

--- src/Pages/Home/IceCreams.js
+++ src/Pages/Home/IceCreams.js
@@ -3,20 +3,16 @@ import React from 'react';
 const IceCreams = ({ icecream }) => {
     const { image, flavour, name, price, about } = icecream;
     return (
-            <div className="card w-96 m-4 bg-amber-200 shadow-xl">
-                <figure className="px-10 pt-10">
-                    <img src={image} alt={name} className="border-lime-600 rounded-xl" />
-                </figure>
-                <div className="card-body text-lime-700">
-                    <h2 className="card-title">{name}</h2>
-                    <p>Price: ${price}</p>
-                    <p className='badge badge-outline bg-amber-900 text-amber-200'>{flavour}</p>
-                    <p className='text-justify'>{about}</p>
-                    <div className="card-actions">
-                        <button className="btn bg-lime-800 w-full">Buy Now</button>
-                    </div>
-                </div>
+        <div class="m-8">
+            <div className="w-96">
+                <img className='border rounded-lg border-4' src={image} alt="" />
             </div>
+            <div className='text-center'>
+                <p className='text-2xl font-bold font-serif text-amber-800'>{name}</p>
+                <p className='text-2xl font-serif text-lime-800'>${price}</p>
+                <button className='btn w-full bg-lime-800'>Buy Now</button>
+            </div>
+        </div>
     );
 };
 
--- src/Pages/Home/Shop.js
+++ src/Pages/Home/Shop.js
@@ -14,7 +14,7 @@ const Shop = () => {
             <h1 className='text-center text-4xl text-lime-700 font-bold m-12 font-serif'>Icecreams!</h1>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
                 {
-                    icecreams.slice(0, 6).map(icecream => <IceCreams icecream={icecream}></IceCreams>)
+                     icecreams.slice(0, 6).map(icecream => <IceCreams icecream={icecream}></IceCreams>)
                 }
             </div>
 
