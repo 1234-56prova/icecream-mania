51ca0c5a3cd45d228e82d17925236fe3c749b8ba
Ilma <ilmaprova13@gmail.com>
2022-07-15 11:51:11 +0600

    useIcecream hook added

--- src/Pages/Home/Shop.js
+++ src/Pages/Home/Shop.js
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import useIcecream from '../../hooks/useIcecream';
 import IceCreams from './IceCreams';
 
 const Shop = () => {
-    const [icecreams, setIcecreams] = useState([]);
-
-    useEffect(() => {
-        fetch('https://raw.githubusercontent.com/1234-56prova/data/main/icecream.json')
-            .then(res => res.json())
-            .then(data => setIcecreams(data));
-    }, [])
+    const [icecreams] = useIcecream();
     return (
         <div >
             <h1 className='text-center text-4xl text-lime-700 font-bold m-12 font-serif'>Icecreams!</h1>
