93b3089a4762533fb59ffda0a48439ca7055bc1e
Ilma <ilmaprova13@gmail.com>
2022-07-21 11:17:07 +0600

    coffee machine section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import History from './History';
+import Machine from './Machine';
 import Trends from './Trends';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <History></History>
             <Trends></Trends>
+            <Machine></Machine>
         </div>
     );
 };
